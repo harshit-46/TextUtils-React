735f99d36fc2ce7db8306990f8c77754aa80b624
Harshit Gupta <iamharshit999@gmail.com>
2024-02-29 00:17:08 +0530

    Updates

---
+++ precache-manifest.3d03b202b9a7240019fd27680bee8d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f7cf67916f83654cf8eee365f72871",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "31b74b7bf977dcb8152c",
+    "url": "/TextUtils-React/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/TextUtils-React/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/TextUtils-React/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "31b74b7bf977dcb8152c",
+    "url": "/TextUtils-React/static/js/main.ceef9e63.chunk.js"
+  },
+  {
+    "revision": "e8122ccafefab8254f73",
+    "url": "/TextUtils-React/static/js/runtime~main.bfec75dd.js"
+  }
+]);
