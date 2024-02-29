685bd21a7e0ec5d21d39a306fa852a3d87c51a3e
Harshit Gupta <iamharshit999@gmail.com>
2024-02-29 06:28:43 +0530

    DOM added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-//import About from './components/About';
-/*
+import About from './components/About';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-*/
 
 function App() {
   const [mode,setMode] = useState('light');
@@ -43,17 +41,17 @@ function App() {
 
   return (
     <>
-    {/*<Router>*/}
+    <Router>
       <Navbar title="TextUtils" mode={mode} togglemode={togglemode} modetext={modeText} aboutText="About"/>
       <Alert alert={alert}/>
       <div className="container">
-        {/*<Routes>
-          <Route exact path="/about" element={<About/>}></Route>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter your Text to analyze" mode={mode}/>}></Route>
-          </Routes>*/}
-          <TextForm showAlert={showAlert} heading="Enter your Text to analyze" mode={mode}/>
+          <Routes>
+            <Route exact path="/about" element={<About/>}></Route>
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter your Text to analyze" mode={mode}/>}></Route>
+          </Routes>
+          {/*<TextForm showAlert={showAlert} heading="Enter your Text to analyze" mode={mode}/>*/}
       </div>
-      {/*</Router>*/}
+      </Router>
     </>
   );
 }
